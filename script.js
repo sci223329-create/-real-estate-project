67d6650f3dddb8d92bef66f15495ea591e6dfb2b
sci223329-create <sci_223329@omu.edu.ly>
2025-12-12 19:15:21 +0300

    الشكل النهاىي
    >>>>>>>>>

--- script.js
+++ script.js
@@ -3,33 +3,48 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const form = document.querySelector("#contact form");
     form.addEventListener("submit", function(e) {
-        e.preventDefault(); // منع إعادة تحميل الصفحة
+        e.preventDefault(); 
         alert("تم إرسال رسالتك بنجاح! سنقوم بالتواصل معك قريباً.");
     });
 
-    
+    // ============================
+    // 2) عند الضغط على الصور (شراء عقار)
+    // ============================
     const images = document.querySelectorAll("#listings img, #gallery img");
 
     images.forEach(function(img) {
         img.addEventListener("click", function() {
-            const propertyName = img.alt || "العقار"; // اسم العقار من alt
+
+            const propertyName = img.alt || "العقار";
+
             const confirmPurchase = confirm(`هل تريد شراء ${propertyName}؟`);
+
             if(confirmPurchase) {
                 alert(`تم تسجيل رغبتك في شراء ${propertyName}. سنتواصل معك قريباً!`);
             }
         });
     });
-ض
+
+
+    // ============================
+    // 3) عند الضغط على العروض (Confirmation)
+    // ============================
     const offers = document.querySelectorAll("#offers li");
+
     const messages = [
-        "فيلا أحلامك — خصم 10%",
+        "فيلا — خصم 10%",
         "شقة عائلية — سعر خاص 300 د.ل / شهر",
         "محل تجاري — فرصة استثمارية مع استشارة مجانية"
     ];
 
-    offers.forEach((offer, index) => {
-        offer.addEventListener("click", () => {
-            alert("تم  اخترت العرض سنقوم بالتواصل معك قريبا : " + messages[index]);
+    offers.forEach((offers, index) => {
+        offers.addEventListener("click", () => {
+
+            let confirmOffer = confirm(`هل تريد اختيار العرض التالي؟\n\n${messages[index]}`);
+
+            if (confirmOffer) {
+                alert(`تم تسجيل رغبتك في عرض: ${messages[index]}. سنقوم بالتواصل معك قريباً!`);
+            }
         });
     });
 
